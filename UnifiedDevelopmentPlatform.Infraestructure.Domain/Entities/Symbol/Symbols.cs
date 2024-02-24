8fd27eeaee8e76af1e25793ceb9dcc59e6d4965a
Claudiomildo Ventura <claudiomildo@hotmail.com>
2024-02-24 20:07:40 -0300

    0058 - 02/24/2024 - The creation of the Symbols class.

---
+++ UnifiedDevelopmentPlatform.Infraestructure.Domain/Entities/Symbol/Symbols.cs
@@ -0,0 +1,118 @@
+﻿namespace UnifiedDevelopmentPlatform.Infraestructure.Domain.Entities.Symbol
+{
+    /// <summary>
+    /// Symbols of Unified Development Platform.
+    /// </summary>
+    public static class Symbols
+    {
+        /// <summary>
+        /// The scape sequence single quote. Unicode encoding = 0x0027
+        /// </summary>
+        public static string SingleQuote => "\'";
+
+        /// <summary>
+        /// The scape sequence double quote. Unicode encoding = 0x0022
+        /// </summary>
+        public static string DoubleQuote => "\"";
+
+        /// <summary>
+        /// The scape sequence backslash. Unicode encoding = 0x005C
+        /// </summary>
+        public static string Backslash => "\\";
+
+        /// <summary>
+        /// The scape sequence null. Unicode encoding = 0x0000
+        /// </summary>
+        public static string Null => "\0";
+
+        /// <summary>
+        /// The scape sequence alert. Unicode encoding = 0x0007
+        /// </summary>
+        public static string Alert => "\a";
+
+        /// <summary>
+        /// The scape sequence backspace. Unicode encoding = 0x0008
+        /// </summary>
+        public static string Backspace => "\b";
+
+        /// <summary>
+        /// The scape sequence form feed. Unicode encoding = 0x000C
+        /// </summary>
+        public static string Formfeed => "\f";
+
+        /// <summary>
+        /// The scape sequence new line. Unicode encoding = 0x000A
+        /// </summary>
+        public static string NewLine => "\n";
+
+        /// <summary>
+        /// The scape sequence carriage return. Unicode encoding = 0x000D
+        /// </summary>
+        public static string CarriageReturn => "\r";
+
+        /// <summary>
+        /// The scape sequence horizontal tab. Unicode encoding = 0x0009
+        /// </summary>
+        public static string HorizontalTab => "\t";
+
+        /// <summary>
+        /// The scape sequence vertical tab. Unicode encoding = 0x000B
+        /// </summary>
+        public static string VerticalTab => "\t";
+
+        /// <summary>
+        /// The comma.
+        /// </summary>
+        public static string Comma => ",";
+
+        /// <summary>
+        /// The semi colon.
+        /// </summary>
+        public static string SemiColon => ";";
+
+        /// <summary>
+        /// The colon.
+        /// </summary>
+        public static string Colon => ":";
+
+        /// <summary>
+        /// The greater than.
+        /// </summary>
+        public static string GreaterThan => ">";
+
+        /// <summary>
+        /// The less than.
+        /// </summary>
+        public static string LessThan => "<";
+
+        /// <summary>
+        /// The left parenthese.
+        /// </summary>
+        public static string LeftParenthese => ")";
+
+        /// <summary>
+        /// The right parenthese.
+        /// </summary>
+        public static string RightParenthese => "(";
+
+        /// <summary>
+        /// The left curly brackets.
+        /// </summary>
+        public static string LeftCurlyBrackets => "}";
+
+        /// <summary>
+        /// The right curly brackets.
+        /// </summary>
+        public static string RightCurlyBrackets => "{";
+
+        /// <summary>
+        /// The left square bracket.
+        /// </summary>
+        public static string LeftSquareBracket => "]";
+
+        /// <summary>
+        /// The right square bracket.
+        /// </summary>
+        public static string RightSquareBracket => "[";
+    }
+}
